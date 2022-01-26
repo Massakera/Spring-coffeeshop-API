9f37d4e5ba173cf62cecdf47edeeaf198df0841f
Massakera <enzomassaki@hotmail.com>
2022-01-26 10:54:49 -0300

    setting coffee class

--- apitest/src/main/java/com/example/apitest/ApitestApplication.java
+++ apitest/src/main/java/com/example/apitest/ApitestApplication.java
@@ -1,5 +1,7 @@
 package com.example.apitest;
 
+import java.util.UUID;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,4 +12,31 @@ public class ApitestApplication {
 		SpringApplication.run(ApitestApplication.class, args);
 	}
 
+}
+
+class Coffee {
+	private final String id;
+	private String name;
+
+	public Coffee(String id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	public Coffee(String name) {
+		this(UUID.randomUUID().toString(), name);
+
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 }
