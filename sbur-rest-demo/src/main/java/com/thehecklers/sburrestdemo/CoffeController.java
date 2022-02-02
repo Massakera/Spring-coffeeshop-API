5445d498d1cf9f5582a1b91cc5f39eac33f67374
Massakera <enzomassaki@hotmail.com>
2022-02-02 13:23:18 -0300

    adding delete method

--- sbur-rest-demo/src/main/java/com/thehecklers/sburrestdemo/CoffeController.java
+++ sbur-rest-demo/src/main/java/com/thehecklers/sburrestdemo/CoffeController.java
@@ -4,11 +4,17 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/coffees")
 class RestApiDemoController {
     private List<Coffee> coffees = new ArrayList<>();
 
@@ -22,12 +28,12 @@ class RestApiDemoController {
     }
 
 
-    @GetMapping("/coffees")
+    @GetMapping
     Iterable<Coffee> getCoffees() {
         return coffees;
     }
 
-    @GetMapping("/coffees/{id}")
+    @GetMapping("/{id}")
     Optional<Coffee> getCoffeeById(@PathVariable String id) {
         for (Coffee c: coffees) {
             if (c.getId().equals(id)) {
@@ -38,13 +44,13 @@ class RestApiDemoController {
         return Optional.empty();
     }
 
-    @PostMapping("/coffees")
+    @PostMapping
     Coffee postCoffee(@RequestBody Coffee coffee) {
         coffees.add(coffee);
         return coffee;
     }
 
-    @PutMapping("/coffees/{id}")
+    @PutMapping("/{id}")
     Coffee putCoffee(@PathVariable String id, @RequestBody Coffee coffee) {
         int coffeeIndex = -1;
 
@@ -56,5 +62,10 @@ class RestApiDemoController {
         }
 
         return (coffeeIndex == -1) ? postCoffee(coffee) : coffee;
+    }
+
+    @DeleteMapping("/{id}")
+    void deleteCoffee(@PathVariable String id) {
+        coffees.removeIf(c -> c.getId().equals(id));
     }
 }
