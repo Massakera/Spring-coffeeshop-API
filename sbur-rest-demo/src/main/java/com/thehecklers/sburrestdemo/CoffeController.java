9db2eb85f9518054c6dca219bca353bb3a63c3ca
Massakera <enzomassaki@hotmail.com>
2022-02-04 13:51:30 -0300

    adding ResponseEntity to PUT method

--- sbur-rest-demo/src/main/java/com/thehecklers/sburrestdemo/CoffeController.java
+++ sbur-rest-demo/src/main/java/com/thehecklers/sburrestdemo/CoffeController.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -51,7 +53,8 @@ class RestApiDemoController {
     }
 
     @PutMapping("/{id}")
-    Coffee putCoffee(@PathVariable String id, @RequestBody Coffee coffee) {
+    ResponseEntity<Coffee> putCoffee(@PathVariable String id,
+    @RequestBody Coffee coffee) {
         int coffeeIndex = -1;
 
         for (Coffee c: coffees) {
@@ -61,7 +64,9 @@ class RestApiDemoController {
             }
         }
 
-        return (coffeeIndex == -1) ? postCoffee(coffee) : coffee;
+        return (coffeeIndex == -1) ?
+                new ResponseEntity<>(postCoffee(coffee), HttpStatus.CREATED) :
+                new ResponseEntity<>(coffee, HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
